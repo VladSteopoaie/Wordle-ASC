f453c73036bd100032a765200b944c9dee0265b0
RK13 <eduard_barbu_13@yahoo.com>
2022-11-01 14:43:28 +0200

    Wordle in Python
    Am facut un Wordle de baza in Python. Am pastrat si folderul cu wordle in C++, in caz ca ne intoarcem la C++. Am pus si in TODO ce ar mai trebui facut pt wordle-ul in python. Daca mai sunt alte chestii ce tb facute pt wordle, spuneti-le in TODO

--- Folder/Wordle_Game/WordleGameSolution/WordleGame/WordleGame.cpp
+++ Folder/Wordle_Game/WordleGameSolution/WordleGame/WordleGame.cpp
@@ -2,11 +2,49 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <cstring>
+#include <vector>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+void checker(char s)
+{
+	int ok = 1;
+	if (strlen(sol) != 5)
+		cout << "Your word doesn't have 5 letters!"
+		for (int i = 0; i < 10; i++)
+			if (strcmp(sol, cuv[i]) != 0)
+				ok = 0;
+	if (ok == 0)
+		cout << "This is not a word from the dictionary! wtf bro";
+	else
+		cout << "Good!"
+	
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    return 0;
+	char cuv[10][6];
+	char guess,sol;
+	int i, tries = 0;
+	strcmp(cuv[0], "cizma");
+	strcmp(cuv[1], "cadou");
+	strcmp(cuv[2], "arena");
+	strcmp(cuv[3], "aerat");
+	strcmp(cuv[4], "musca");
+	cout<<"The game starts!"
+	run_game = 1;
+		while (run_game)
+		cout << "Number of tries: " << tries << '\n';
+		cin >> sol;
+		tries++;
+	
+		
+	
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
